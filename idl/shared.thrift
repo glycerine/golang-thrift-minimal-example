59c23deb93c348e545dfad3f1a88f20d9d151497
Jason E. Aten <j.e.aten@gmail.com>
2015-10-18 05:08:38 +0000

    don't require dart or haxe install

--- idl/shared.thrift
+++ idl/shared.thrift
@@ -24,11 +24,9 @@
 
 namespace cpp shared
 namespace d share // "shared" would collide with the eponymous D keyword.
-namespace dart shared
 namespace java shared
 namespace perl shared
 namespace php shared
-namespace haxe shared
 
 struct SharedStruct {
   1: i32 key
